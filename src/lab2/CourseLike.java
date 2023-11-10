0733ecd513e9210ad7c1531e08ffcaf2ec02ab14
breyes4 <71798440+breyes4@users.noreply.github.com>
2023-11-09 22:41:46 -0600

    CourseLike.java

---
+++ src/lab2/CourseLike.java
@@ -0,0 +1,16 @@
+package lab2;
+
+public interface CourseLike {
+
+    String getCourseName();
+
+    void setCourseName(String courseName);
+
+    String getCourseNumber();
+
+    void setCourseNumber(String courseNumber);
+
+    double getCredits();
+
+    void setCredits(double credits);
+}
